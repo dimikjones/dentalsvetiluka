fcaab29c33b57a4e1f6b1244da5a766fa8e2c833
Marko Dimitrijevic <mdimitrijevic84@gmail.com>
2025-07-24 17:48:07 +0200

    Remove allsmiles_core_team_has_single function.

--- inc/customization/include.php
+++ inc/customization/include.php
@@ -4,12 +4,3 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-/**
- * Set single team post to false - override default which is true
- *
- * @return bool
- */
-function allsmiles_core_team_has_single() {
-	return false;
-}
